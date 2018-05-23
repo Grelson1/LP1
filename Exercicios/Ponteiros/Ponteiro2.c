d0e56b2de551839270dc7fbbb6b1a1d2e89bc455
Renato Neves <38740676+RenatoESN@users.noreply.github.com>
2018-05-23 17:01:46 -0300

    Update Ponteiro2.c

--- Exercicios/Ponteiros/Ponteiro2.c
+++ Exercicios/Ponteiros/Ponteiro2.c
@@ -1,4 +1,3 @@
-//A ser consertado
 #include <stdio.h>
 
 void compra (int* conta, int valor){
@@ -7,22 +6,18 @@ void compra (int* conta, int valor){
 int main (void){
     int conta1;
     int conta2;
-    int* conta;
-    int valor;
+   
     printf ("Digite o saldo da conta 1: ");
     scanf ("%d\n", &conta1);
     printf ("Digite o saldo da conta 2: ");
     scanf ("%d\n", &conta2);
     if (conta1 > conta2){
-        *conta = conta1;
+       compra(&conta1, 500);
     }
     else{
-        *conta = conta2;
+        compra(&conta2, 500);
     }
-    printf ("Digite o valor a ser debitado: ");
-    scanf ("%d\n", &valor);
-    compra (conta, valor);
-    printf ("Saldo da conta 1: %d", conta1);
-    printf ("Saldo da conta 2: %d", conta2);
+    printf ("Saldo da conta 1: %d\n", conta1);
+    printf ("Saldo da conta 2: %d\n", conta2);
     return 0;
 }
