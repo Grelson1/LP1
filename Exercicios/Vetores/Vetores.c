39ef71539998328814253c9c20aec703c685debb
Renato Neves <38740676+RenatoESN@users.noreply.github.com>
2018-06-13 14:32:38 -0300

    Update Vetores.c

--- Exercicios/Vetores/Vetores.c
+++ Exercicios/Vetores/Vetores.c
@@ -1,22 +1,16 @@
-//A ser consertado
 #include <stdio.h>
 
 void preenche (int* vec, int n){
-    int a;
-    int i;
+    int a, i, j;
     for (i = 0; i < n; i++){
         printf ("Digite um valor: ");
-        scanf ("%d\n", &a);
+        scanf ("%d", &a);
         vec[i] = a;
-    int j;
-    for (j = 0; j < 5; j++){
-        prinft("%p\n", vec[j]);
     }
 }
 
 int main (void){
     int vec[5];
-    preenche ( vec, 5);
-    }
+    preenche (vec, 5);
     return 0;
 }
