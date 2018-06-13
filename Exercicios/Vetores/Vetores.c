bee5091b9b606d5d6e35fb56a49f76077ea1d03f
Renato Neves <38740676+RenatoESN@users.noreply.github.com>
2018-06-13 14:39:18 -0300

    Update Vetores.c

--- Exercicios/Vetores/Vetores.c
+++ Exercicios/Vetores/Vetores.c
@@ -8,9 +8,20 @@ void preenche (int* vec, int n){
         vec[i] = a;
     }
 }
+void media (int* vec, int n){
+    int soma = 0;
+    int cont = 0;
+    for (int i = 0; i < n; i++){
+        soma = soma + vec[i];
+        cont++;
+    }
+    int med = soma/cont;
+    printf ("Valor medio: %d", med);
+}
 
 int main (void){
     int vec[5];
     preenche (vec, 5);
+    media(vec, 5);
     return 0;
 }
