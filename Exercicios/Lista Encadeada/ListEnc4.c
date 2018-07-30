ffac180897b799a1b08338f7c11d62e17079f322
Renato Neves <38740676+RenatoESN@users.noreply.github.com>
2018-07-30 14:55:55 -0300

    Create ListEnc4.c

---
+++ Exercicios/Lista Encadeada/ListEnc4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+struct Caixa{
+	int valor;
+	struct Caixa* prox;
+};
+
+void exibe (struct Caixa* caixa){
+    printf("->%d", caixa->valor);
+}
+int main (void){
+	struct Caixa c0, c1, c2, c3, c4, cab;
+	c4.valor = 5;
+	c4.prox = NULL;
+	c3.valor = 7;
+	c3.prox = &c4;
+	c2.valor = 9;
+	c2.prox = &c3;
+	c1.valor = 3;
+	c1.prox = &c2;
+	c0.valor = 1;
+	c0.prox = &c1;
+	cab.valor = NULL;
+	cab.prox = &c0;
+	exibe (&c0);
+    exibe (&c1);
+	exibe (&c2);
+	exibe (&c3);
+	exibe (&c4);
+	c1.prox = &c3;
+	printf("\n");
+	exibe (&c0);
+	exibe (&c2);
+	exibe (&c3);
+	exibe (&c4);
+}
